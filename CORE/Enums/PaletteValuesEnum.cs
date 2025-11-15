455b7f99f9a26de4ee42434121614883c43c1e0b
Lisyaka46 <90254409+Lisyaka46@users.noreply.github.com>
2025-11-15 10:56:18 +0300

    Добавление объекта палитры и словаря свойств Q-даты для настройки отображения объекта

---
+++ CORE/Enums/PaletteValuesEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationOperPageLes.CORE.Enums
+{
+    internal enum PaletteValuesEnum : uint
+    {
+        BG_Red = 0u,
+        BB_Red = 1u,
+        FG_Red = 2u,
+
+        BG_PastelBlue = 3u,
+        BB_PastelBlue = 4u,
+        FG_PastelBlue = 5u,
+    }
+}
---
+++ CORE/Settings/PaletteQDataSetting.cs
@@ -0,0 +1,41 @@
+﻿using ApplicationOperPageLes.CORE.Enums;
+using ApplicationOperPageLes.CORE.Struct;
+using IEL.CORE.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using OPRES = ApplicationOperPageLes.Properties.Resources;
+
+namespace ApplicationOperPageLes.CORE.Settings
+{
+    internal class PaletteQDataSetting
+    {
+        public PaletteQDataSetting()
+        {
+            Resource = new() { Source = new(StructDirectoryResources.GetResourcePath(nameof(OPRES.PaletteDictionary))) };
+            DictionaryQData = Resource.Keys.Cast<string>().ToDictionary(i => i, i => (QData)Resource[i]);
+        }
+
+        /// <summary>
+        /// Объект ресурса данных палитры
+        /// </summary>
+        private ResourceDictionary Resource;
+
+        /// <summary>
+        /// Объект словаря всех данных Q-логики
+        /// </summary>
+        private Dictionary<string, QData> DictionaryQData;
+
+        /// <summary>
+        /// Получить значение данных впектров цвета Q-логики
+        /// </summary>
+        /// <param name="PaletteValue">Значение пересичления представляющее название ресурса</param>
+        /// <returns></returns>
+        /// <exception cref="Exception">Исключение при отсутствующем ресурсе в словаре</exception>
+        public QData GetQdataFromEnum(PaletteValuesEnum PaletteValue) => DictionaryQData[Enum.GetName(PaletteValue) ??
+            throw new Exception("Ресурс под заданным именем не содержится в палитре Q-логики")];
+    }
+}
