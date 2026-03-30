411a344470e1de3f216658b5db237dac0ad56cd4
Lisyaka46 <90254409+Lisyaka46@users.noreply.github.com>
2026-03-30 22:43:24 +0300

    Модернизация кода с учётом дерева зависимостей

--- UI/Windows/MainWindow.xaml.cs
+++ UI/Windows/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
