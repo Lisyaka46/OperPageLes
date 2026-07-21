b0494f56eee859eeb3b33969b03b646890d11994
Lisyaka46 <90254409+Lisyaka46@users.noreply.github.com>
2026-07-21 18:57:55 +0300

    Обновление отображения контактов автора в языковых переводах

--- UI/UserElementsControl/Default/OPLLanguageButton.xaml.cs
+++ UI/UserElementsControl/Default/OPLLanguageButton.xaml.cs
@@ -6,10 +6,13 @@ using OPLAPI.CORE.Animation;
 using OPLAPI.CORE.Interfaces;
 using OPLAPI.CORE.Language;
 using OPLAPI.CORE.Person;
+using OPLAPI.OIEL.UserElementsControl;
 using System.Diagnostics;
 using System.Security.Policy;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
 using OPRES = OperPageLes.Properties.Resources;
 
 namespace OperPageLes.UI.UserElementsControl.Default
@@ -29,7 +32,7 @@ namespace OperPageLes.UI.UserElementsControl.Default
             set
             {
                 _DataContext = value;
-                UpdateVisualData();
+                Dispatcher.Invoke(UpdateVisualData);
             }
         }
 
@@ -51,26 +54,39 @@ namespace OperPageLes.UI.UserElementsControl.Default
         public OPLLanguageButton()
         {
             InitializeComponent();
-            _DataContext = LangInfo.Inknown;
+            _DataContext = LangInfo.Unknown;
             StackVisualContacts = new()
             {
-                Width = 0d,
-                Background = IELButtonContacts.SourceForeground.SourceBrush,
-                Margin = new(0, 0, 8, 0),
                 Orientation = System.Windows.Controls.Orientation.Horizontal,
                 HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
+                ClipToBounds = false,
             };
-            UpdateVisualData();
+            BorderContacts.Width = 0d;
+            BorderContacts.Child = StackVisualContacts;
             TextBlockNameLanguage.Foreground = SourceForeground.SourceBrush;
             TextBlockVersionLanguage.Foreground = SourceForeground.SourceBrush;
             TextBlockAutor.Foreground = SourceForeground.SourceBrush;
             Lang.LanguageUpdated += Lang_LanguageUpdated;
+            IELButtonContacts.Source = StructDirectoryResources.GetResourceBitmap(nameof(OPRES.ContactInfo));
             IELButtonContacts.OnActivateMouseLeft += (sender, e) =>
             {
-                OPLAnimationManager.AnimateTakingZeroTo(ManagerAnimation, StackVisualContacts, WidthProperty,
-                    ActiveVisualConracts ? 0d : StackVisualContacts.ActualWidth, TimeSpan.FromMilliseconds(460d));
-                ActiveVisualConracts = !ActiveVisualConracts;
+                ChangeVisibleContacts();
+                e.Handled = true;
             };
+            MouseLeave += (sender, e) =>
+            {
+                if (ActiveVisualConracts) ChangeVisibleContacts();
+            };
+        }
+
+        /// <summary>
+        /// Изменить вид отображения контактов автора
+        /// </summary>
+        internal void ChangeVisibleContacts()
+        {
+            OPLAnimationManager.AnimateTakingZeroTo(ManagerAnimation, BorderContacts, WidthProperty,
+                    ActiveVisualConracts ? 0d : StackVisualContacts.ActualWidth, TimeSpan.FromMilliseconds(460d));
+            ActiveVisualConracts = !ActiveVisualConracts;
         }
 
         /// <summary>
@@ -84,7 +100,7 @@ namespace OperPageLes.UI.UserElementsControl.Default
         /// <summary>
         /// Обновить отображение данных в объекте от DataContext
         /// </summary>
-        private void UpdateVisualData()
+        private async Task UpdateVisualData()
         {
             try { ImageBrushImageLanguage.ImageSource = StructDirectoryResources.GetResourceBitmap($"Flag{DataContext.Config.Locate}"); }
             catch { ImageBrushImageLanguage.ImageSource = StructDirectoryResources.GetResourceBitmap(nameof(OPRES.World)); }
@@ -93,48 +109,92 @@ namespace OperPageLes.UI.UserElementsControl.Default
             TextBlockVersionLanguage.Text = DataContext.Config.Version;
             RunTextAutorName.Text = DataContext.LangAutor.Name;
             IELButtonContacts.IsEnabled = DataContext.LangAutor.Contacts.Length > 0;
-            UpdateVisualContacts(DataContext.LangAutor.Contacts);
+            await UpdateVisualContacts(DataContext.LangAutor.Contacts);
         }
 
         /// <summary>
         /// Обновить отображение списка контактов
         /// </summary>
         /// <param name="SourceContacts">Новый список контактов</param>
-        private void UpdateVisualContacts(Contact[] SourceContacts)
+        private async Task UpdateVisualContacts(Contact[] SourceContacts)
         {
-            IELButtonImage ButtonContact;
-            for (int i = 0; i < SourceContacts.Length; i++)
+            IELButtonContacts.IsEnabled = false;
+            if (SourceContacts.Length == 0) return;
+            if (ActiveVisualConracts)
             {
-                ButtonContact = GenerateButtonContact(SourceContacts[i]);
+                OPLAnimationManager.AnimateTakingZeroTo(ManagerAnimation, BorderContacts, WidthProperty,
+                    0d, TimeSpan.FromMilliseconds(460d));
+                await Task.Delay(600);
             }
+            OPLVisualElementIM ButtonContact;
+            int i;
+            for (i = 0; i < SourceContacts.Length; i++)
+            {
+                if (i < StackVisualContacts.Children.Count)
+                {
+                    ButtonContact = (OPLVisualElementIM)StackVisualContacts.Children[i];
+                    ButtonContact.DataContext = SourceContacts[i];
+                }
+                else
+                {
+                    ButtonContact = GenerateButtonContact(SourceContacts[i]);
+                    StackVisualContacts.Children.Add(ButtonContact);
+                }
+            }
+            if (i < StackVisualContacts.Children.Count)
+                StackVisualContacts.Children.RemoveRange(i + 1, StackVisualContacts.Children.Count - i);
+            if (ActiveVisualConracts)
+            {
+                OPLAnimationManager.AnimateTakingZeroTo(ManagerAnimation, BorderContacts, WidthProperty,
+                    StackVisualContacts.ActualWidth, TimeSpan.FromMilliseconds(1460d));
+            }
+            IELButtonContacts.IsEnabled = true;
         }
 
         /// <summary>
         /// Обновить отображение списка контактов
         /// </summary>
         /// <param name="SourceContacts">Новый список контактов</param>
-        private static IELButtonImage GenerateButtonContact(Contact SourceContact)
+        private OPLVisualElementIM GenerateButtonContact(Contact SourceContact)
         {
-            IELButtonImage Result = new()
+            OPLVisualElementIM Result = new()
             {
-                DataContext = SourceContact,
+                ManagerAnimation = ManagerAnimation,
+                PaletteElement = App.CurrentApp.ActiveThemeApplication[CORE.Enums.PaletteSpectrumEnum.Terracotta],
+                Margin = new(-8d, -5d, -8d, -5d),
+                CornerRadius = new(15d),
+                BorderThickness = new(2d),
+                VisualOrientationName = OrientationName.Up,
+                Text = SourceContact.Mask,
+                FontSize = 9d,
+                OffsetBorderNaming = 24d,
             };
+            Result.SetSizeIconApp(new(20d, 20d));
             Result.OnActivateMouseLeft += (sender, e) =>
             {
                 Contact Source = (Contact)((FrameworkElement)sender).DataContext;
                 Process.Start(new ProcessStartInfo(Source.URL) { UseShellExecute = true });
+                e.Handled = true;
             };
             Result.DataContextChanged += async (sender, e) => await ContactButton_DataContextChanged(sender, e);
+            Result.DataContext = SourceContact;
             return Result;
         }
 
         /// <summary>
         /// Обработчик события изменения опорных данных для отображения контакта
         /// </summary>
-        private static async Task ContactButton_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        private async Task ContactButton_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            Contact Source = (Contact)((FrameworkElement)sender).DataContext;
-            await App.CurrentApp.DownloadFavicon(new(Source.URL));
+            OPLVisualElementIM Sender = (OPLVisualElementIM)sender;
+            Contact Source = (Contact)Sender.DataContext;
+            Sender.IsEnabled = false;
+            Sender.Text = Source.Mask;
+            Sender.Source = StructDirectoryResources.GetResourceBitmap(nameof(OPRES.World));
+            try { Sender.Source = await App.CurrentApp.DownloadFavicon(new(Source.URL)); } catch { }
+            Sender.IsEnabled = true;
+            OPLAnimationManager.AnimateTakingZeroTo(ManagerAnimation, Sender, OPLVisualElementIM.OpacityProperty,
+                1d, TimeSpan.FromMilliseconds(600d));
         }
     }
 }
