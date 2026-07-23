e815e2004c29e0bf31a990a1f81d3410ec797118
Lisyaka46 <90254409+Lisyaka46@users.noreply.github.com>
2026-07-23 21:20:52 +0300

    Добавление новой страницы генерации языкового перевода

---
+++ UI/Pages/Browser/PageLanguageGenerator.xaml.cs
@@ -0,0 +1,47 @@
+﻿using OperPageLes.CORE.Enums.Language;
+using OPLAPI.CORE.Language;
+using OPLAPI.OIEL.CORE.Browser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace OperPageLes.UI.Pages.Browser
+{
+    /// <summary>
+    /// Логика взаимодействия для PageLanguageGenerator.xaml
+    /// </summary>
+    public partial class PageLanguageGeneratorApp : PageBrowser
+    {
+        public PageLanguageGeneratorApp()
+        {
+            InitializeComponent();
+            Lang.LanguageUpdated += Lang_LanguageUpdated;
+        }
+
+        /// <summary>
+        /// Обработчик обновления языкового перевода
+        /// </summary>
+        private void Lang_LanguageUpdated(object? sender, EventArgs e)
+        {
+            TextBlockLanguageLocate.Text = Lang.GetValue(LangUITranslate.Localization);
+            TextBlockLanguageName.Text = Lang.GetValue(LangUITranslate.Naming);
+            TextBlockLanguageVersion.Text = Lang.GetValue(LangUITranslate.Naming);
+            TextBlockSourceLanguage.Text = Lang.GetValue(LangGenDictionaryTranslate.SourceLanguage);
+            TextBlockLanguagePathSave.Text = Lang.GetValue(LangUITranslate.DirectorySave);
+            IELButtonAutors.Text = Lang.GetValue(LangUITranslate.Autors);
+            IELButtonOverview.Text = Lang.GetValue(LangUITranslate.Overview);
+            //IELButtonManipulation.Text = Lang.GetValue(LangUITranslate.Autors);
+            IELButtonCancel.Text = Lang.GetValue(LangUITranslate.Cancel);
+        }
+    }
+}
